bff477e9f9f3f4ccc51228ece2eb8bb7a6909b0c
hash0131 <hash0131@algonquinlive.com>
2021-06-12 02:01:32 -0400

    lab3
    lab3

---
+++ lab3/Lab3/Controllers/Home/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab3.Models;
+
+
+namespace Lab3.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        public IActionResult Count(Person person)
+        {
+            return View(person);
+        }
+
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayPerson(Person person)
+        {
+            return View(person);
+        }
+    }
+}
